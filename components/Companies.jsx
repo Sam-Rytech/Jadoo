0f4905a79be165444c8f0b5eb92e17b0403c6e6a
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-15 00:22:04 +0100

    updated the image src import

--- components/Companies.jsx
+++ components/Companies.jsx
@@ -1,10 +1,10 @@
 export default function Companies() {
   return (
     <section className="flex flex-wrap justify-center gap-12 px-4 py-12 mx-auto max-w-7xl">
-      {['axon', 'jetstar', 'expedia', 'qantas', 'alitalia'].map((logo) => (
+      {['Axon', 'Jetstar', 'Expedia', 'Qantas', 'Alitalia'].map((logo) => (
         <img
           key={logo}
-          src={`/images/${logo}.png`}
+          src={`/${logo}.png`}
           alt={logo}
           className="h-8 transition grayscale hover:grayscale-0"
         />
